11d20ce4a00f1792e32f51a5b5127eb0f2779cf7
Sarah S <srhsmn@gmail.com>
2013-03-13 21:52:12 -0400

    sooo, shadows are elusive. if only i could see them....
    I started importing sim.wall and knew i was doing things wrong. look,
    i'm learning!

---
+++ src/sim/wall.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sim;
+
+import com.jme3.renderer.queue.RenderQueue.ShadowMode;
+
+/**
+ *
+ * @author Simon
+ */
+class wall {
+
+    static void setShadowMode(ShadowMode shadowMode) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
